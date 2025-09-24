734dfe025e240caff11d935404336ca0d2802045
doochim <dookudumb@gmail.com>
2025-09-24 16:25:02 +0700

    PostgreSQL setup and contacts migration

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -88,9 +88,9 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'truongthanhweb'),
-            'username' => env('DB_USERNAME', 'dooku'),
-            'password' => env('DB_PASSWORD', '123'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
